b0d322ac7af7822cf2a6165e17920b943fbfd1e0
Adriano-Rueda <ruedaadriano7@gmail.com>
2026-02-04 12:26:59 +0100

    fixed defect_rate dashboards and added dashboard import files

--- Pluto/utils/defect_rate_medio.py
+++ Pluto/utils/defect_rate_medio.py
@@ -3,6 +3,7 @@ from utils.fetch_data import (
     fetch_defect_rate_detailed_data
 )
 import numpy as np
+import re
 
 def get_defect_rate_medio():
     total_bugs, client_reported_bugs = fetch_defect_rate_data()
@@ -23,13 +24,21 @@ def get_defect_rate_medio():
 def get_defect_rate_per_prodotto():
     total_bugs_df,client_reported_bugs_df = fetch_defect_rate_detailed_data()
 
-    df_ratio = client_reported_bugs_df / total_bugs_df
+    df_ratio = round((client_reported_bugs_df / total_bugs_df),1)
 
-    df = df_ratio.replace([np.inf, -np.inf,np.nan], None)
+    df = df_ratio.replace([np.inf, -np.inf,np.nan], "No Bugs Reported")
     df = df.where(df.notna(), None)
     
-    df =  df.to_dict(orient="index")
-    return df
+    defect_dict =  df.to_dict(orient="index")
+    defect_list = []
+    for key,val in defect_dict.items():
+        new_item = {}
+        
+        new_item["date"] = key
+        for k,v in val.items():
+            new_item[k] = v
+        defect_list.append(new_item )
+    return defect_list
 
 
 
